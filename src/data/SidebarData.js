6989ad3212e2ed5be6e7c25f86b615f009b0be6a
Thai Do <thai.hl.do@gmail.com>
2021-02-23 13:12:31 -0800

    working on sidebar component

--- src/data/SidebarData.js
+++ src/data/SidebarData.js
@@ -3,9 +3,9 @@ import InboxIcon from '@material-ui/icons/Inbox'
 import DraftsIcon from '@material-ui/icons/Drafts'
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
 import PeopleIcon from '@material-ui/icons/People'
-import AppsIcon from '@material-ui/iconsApps'
+import AppsIcon from '@material-ui/icons/Apps'
 
-export const sidebarItems = [
+export const sidebarItemsData = [
   {
     icon: <MessageIcon />,
     text: 'Thread',
