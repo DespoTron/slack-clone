72cfe189bc46bb4529cd5b0226ed20edb58f41ec
Thai Do <thai.hl.do@gmail.com>
2021-02-23 11:47:07 -0800

    cleaned folder structure and added styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg'
 import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Chat from './components/Chat'
 import Login from './components/Login'
+import Header from './components/Header'
+import styled from 'styled-components'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path="/room">
-            <Chat />
-          </Route>
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
+        <Container>
+          <Header />
+          <Switch>
+            <Route path="/room">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </Container>
       </Router>
     </div>
   )
 }
 
 export default App
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: orange;
+`
