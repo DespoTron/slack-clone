51ff19f2a0bf1b994ceb2e3687b19750bdfcff32
Thai Do <thai.hl.do@gmail.com>
2021-02-24 14:08:18 -0800

    working on chat message component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
 import ChatInput from './ChatInput'
+import ChatMessage from './ChatMessage'
 
 function Chat() {
   return (
@@ -18,7 +19,9 @@ function Chat() {
           <Info />
         </ChannelDetails>
       </Header>
-      <MessageContainer></MessageContainer>
+      <MessageContainer>
+        <ChatMessage />
+      </MessageContainer>
       <ChatInput />
     </Container>
   )
